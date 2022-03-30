84b9f172253d08a3e55577b4a7b3a6520c527c68
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-30 13:23:49 +0300

    Done

--- BackgroundThreadSTBC/ContentView.swift
+++ BackgroundThreadSTBC/ContentView.swift
@@ -6,83 +6,66 @@
 //
 
 import SwiftUI
-import CoreData
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray = [String]()
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 2: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 3: \(Thread.isMainThread)")
+                print("CHECK 4: \(Thread.current)")
+            }
+        }
+        
+    }
+    
+    private func downloadData() -> [String] {
+        var data = [String]()
+        
+        for x in 0..<100 {
+            data.append("\(x)")
+                print(data)
+        }
+        return data
+    }
+    
+}
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ScrollView {
+            LazyVStack(spacing: 20) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
                     }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
